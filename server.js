19e99dd0d217eac797d09250dbcc6f30f288e70d
Sarah0079ja <sodiavbara@gmail.com>
2020-04-06 13:21:58 +0100

    fixedmongoose

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 
 // db config
-const db = config.get( "MONGO_URI");
+// const db = process.env.MONGO_URI;
   
-
 // connect mongo  
 mongoose
-  .connect(db, {  
+  .connect("mongodb+srv://MovieDb:movie01@cluster0-9n9ja.mongodb.net/test?retryWrites=true&w=majority"
+  , {  
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
